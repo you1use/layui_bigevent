f1e145f4cf43fbd6ac76be6cfb6e9da8b974166e
zhoujiangping <2660764135@qq.com>
2021-12-26 22:36:25 +0800

    首页模块已完成

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -5,4 +5,21 @@ $.ajaxPrefilter(function(options) {
   // 为所有请求 拼接上完整的路径
   options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
 
+  // 为统一有权限接口 设置 headers 请求头
+  if(options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+  // 全局统一挂载 complete 回调函数
+  options.complete = function(res) {
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 强制清空token
+      localStorage.removeItem('token');
+      // 强制跳转登录页面
+      location.href = 'http://127.0.0.1:5500/layui_bigevent/login.html'
+    }
+  }
 });
---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$(function() {
+  // 调用 gitUserInfo 获取用户基本信息
+  gitUserInfo();
+});
+
+// 获取用户的基本信息
+function gitUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    success: function(res) {
+      // console.log(res);
+      if(res.status !== 0) {
+        return layui.layer.msg(res.message);
+      }
+      // 调用 renderAvater 渲染用户头像和名称
+      renderAvater(res.data);
+    },
+    // 因为每个 有有权限的路径都需要是用 complete 回调函数，所以可以直接在 baseApi.js中全局挂载，不需要重复写
+    // 无论成功或失败，都会调用 该回调函数
+    // complete: function(res) {
+    //   // 在 complete 回调函数中， 可以使用 res.responseJSON 拿到服务器响应回来的数据
+    //   if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+    //     // 强制清空token
+    //     localStorage.removeItem('token');
+    //     // 2. 强制跳转到登录页面
+    //     location.href = 'http://127.0.0.1:5500/layui_bigevent/login.html';
+    //   }
+    // }
+  });
+}
+
+// 渲染用户头像和名称
+function renderAvater(user) {
+  // 获取 该用户的 名称
+  let name = user.nickname || user.username;
+  // 设置欢迎文本内容
+  $('#welcome').html('欢迎 &nbsp;&nbsp;'+ name);
+  // 按需渲染头像
+  // 判断用户是否上传了头像
+  if(user.user_pic !== null) {
+    // 如果没有头像信息，则隐藏 img 标签，显示 .text-avatar盒子
+    $('.layui-nav-img').attr('src', user.user_pic).show();
+    $('.text-avatar').hide();
+  } else {
+    // 获取 名称的首个字符,转换成大写字符
+    let first = name[0].toUpperCase();
+    // 如果没有头像信息，则隐藏 img 标签，显示 .text-avatar盒子
+    $('.layui-nav-img').hide();
+    $('.text-avatar').html(first).show();
+  }
+}
